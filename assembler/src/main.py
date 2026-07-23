325808104abbdae3c03cbbc99423a0419e9fa905
Burrito <165977664+Burritossss@users.noreply.github.com>
2026-07-22 21:00:48 -0600

    started work on making assembler ignoring everything I need to do in the main.py I need to work more on the actual components and currently working on a different file layout for everything.

--- assembler/src/main.py
+++ assembler/src/main.py
@@ -5,3 +5,6 @@ if len(sys.argv) < 2 or sys.argv[1] == '-h':
     '\tpython3 main.py [options] [assembly file path] [binary output path]\n\n'
     'Options:\n'
     '\t-h\t\tDisplays this message')
+
+def main():
+    pass
